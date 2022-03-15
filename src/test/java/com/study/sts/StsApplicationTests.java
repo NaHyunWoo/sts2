998a843aa13596a690bd2287a8463e9055406530
나현우 <nhw0926@naver.com>
2022-03-15 22:02:17 +0900

    Initial commit

---
+++ src/test/java/com/study/sts/StsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
